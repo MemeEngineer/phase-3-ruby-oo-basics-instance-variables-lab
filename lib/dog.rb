e2432eb3355a158fcc375d6601eab89754a103e6
Johnny Wu <johnnyw131@gmail.com>
2022-01-11 01:16:59 -0500

    commit tree 01cef57eb136c404c0abb07bcc1088396ca1dbde

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,24 @@
 class Dog
 
-end
+    #setter method
+    def name=(dogs_name)
+        # assign the @name instance variable a value of whatever is passed as an argument
+      @this_dogs_name = dogs_name
+    end
+  
+     # getter method
+    def name
+         # access the data from the @name instance variable and return it
+      @this_dogs_name
+    end
+  end
+  
+  lassie = Dog.new
+  # call the Dog#name= method with a value of "Lassie"
+  lassie.name = "Lassie"
+  # this is equivalent to calling odie.name=("lassie")
+  puts lassie.name
+  
+
+
+
